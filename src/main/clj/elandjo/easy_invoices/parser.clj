701242eb209d10a093d6b9c661823fb21daa4040
joneland <jonathan.eland@gmail.com>
2016-03-09 14:44:26 +0000

    Breaking apart function so that the smaller functions can be reused between invoice and timesheet

--- src/main/clj/elandjo/easy_invoices/parser.clj
+++ src/main/clj/elandjo/easy_invoices/parser.clj
@@ -1,21 +1,27 @@
 (ns elandjo.easy-invoices.parser
   (:require [clojure.string :as string]))
 
-(def content-keys [:name :client :days-worked :period :company-name :company-address :phone-number :agency-address])
+(def content-keys [:name :client :days-worked-tally :month :company-name :company-address :phone-number :agency-address])
+
+(defn days-worked [timesheet]
+  (->> 
+    (string/split (:days-worked-tally timesheet) #" ")
+    (map #(if (nil? (re-find #"\d+" %)) % (Integer. %)))))
+
+(defn days-in-month [days-worked]
+  (->> 
+    (count days-worked)
+    (inc)
+    (range 1)))
 
 (defn apply-timesheet-formatting [timesheet]
-  (let [days-worked (->> 
-                      (string/split (:days-worked timesheet) #" ")
-                      (map #(if (nil? (re-find #"\d+" %)) % (Integer. %))))
-        days-in-month (->> 
-                        (count days-worked)
-                        (inc)
-                        (range 1))
-        formatted-days-worked (map #(into {} {:day % :time %2}) days-in-month days-worked)
-        formatted-period-worked (format "%d %s - %d %s" (first days-in-month) (:period timesheet) (last days-in-month) (:period timesheet))]
+  (let [days-worked (days-worked timesheet)
+        days-in-month (days-in-month days-worked)
+        day-vs-time-worked (map #(into {} {:day % :time %2}) days-in-month days-worked)
+        period-worked (format "%d %s - %d %s" (first days-in-month) (:month timesheet) (last days-in-month) (:month timesheet))]
     (-> timesheet
-      (assoc :days-worked formatted-days-worked)
-      (assoc :period formatted-period-worked))))
+      (assoc :days-worked day-vs-time-worked)
+      (assoc :period period-worked))))
 
 (defn apply-invoice-formatting [invoice]
   invoice)
@@ -27,8 +33,9 @@
      (select-keys required-details))))
 
 (defn parse-timesheet [input-file]
-  (let [timesheet (extract-contents input-file [:name :client :days-worked :period])]
-    (apply-timesheet-formatting timesheet)))
+  (let [timesheet (extract-contents input-file [:name :client :days-worked-tally :month])]
+    (-> (apply-timesheet-formatting timesheet)
+        (dissoc :month :days-worked-tally))))
 
 (defn parse-invoice [input-file]
   (let [invoice (extract-contents input-file [:company-name :company-address :phone-number :agency-address])] 
